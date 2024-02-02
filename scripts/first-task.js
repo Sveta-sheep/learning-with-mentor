1a5292c3d88341df2da9639f5ca8a1dbb25e05c7
Svitlana <svitlana.vysotska@zealicsolutions.com>
2024-02-02 10:40:16 +0200

    feat: implemented fibonacci and createTime functions

--- scripts/first-task.js
+++ scripts/first-task.js
@@ -23,7 +23,7 @@ const obj = {
     onced
 }
 
-console.log('once: ', obj.onced(2,3)) // 6
+// console.log('once: ', obj.onced(2,3)) // 6
 
 //create functions
 function createFunctions(n) {
@@ -40,8 +40,54 @@ function createFunctions(n) {
 
 var callbacks = createFunctions(5); // create an array, containing 5 functions
 
-console.log('create functions 0 index = ', callbacks[0]()); // must return 0
-console.log('create functions 3 index = ', callbacks[3]()); // must return 3
+// console.log('create functions 0 index = ', callbacks[0]()); // must return 0
+// console.log('create functions 3 index = ', callbacks[3]()); // must return 3
+
+
+//fibonacci
+
+const calculatedNumbers = {};
+
+function fibonacci(n) { // 10
+    
+    if (n < 2)
+      return n;
+
+      if (n in calculatedNumbers) return calculatedNumbers[n];
+
+      calculatedNumbers[n] = fibonacci(n-1) + fibonacci(n-2);
+    
+    return calculatedNumbers[n]; //9 + 8
+  }
+
+  console.log(fibonacci(15)) // 610
+  console.log(fibonacci(50)) // 12586269025
+
+
+// createTimer
+function createTimer() {
+    return {
+        time: 0,
+        start: function() {
+            this.time = Date.now();
+        },
+        getTime: function() {
+            const difference = (Date.now() - this.time) / 1000;
+
+            return difference + 's';
+        }
+    }
+}
+
+const timer = createTimer();
+
+
+// setTimeout(() => {
+//     timer.start()   
+// }, 3000)
+// setTimeout(() => {
+//     console.log(timer.getTime())    
+// }, 6000)
 
 
 // create Person
@@ -63,8 +109,8 @@ function createPerson(name, age) {
 
 const person = createPerson('Svitlana', 23)
 
-console.log('person: ', person)
-console.log('person name: ', person.getName())
-console.log('person age: ', person.getAge())
-person.setAge(25)
-console.log('person new age: ', person.getAge())
+// console.log('person: ', person)
+// console.log('person name: ', person.getName())
+// console.log('person age: ', person.getAge())
+// person.setAge(25)
+// console.log('person new age: ', person.getAge())
